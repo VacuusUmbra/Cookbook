d5671347bd77270069fe63fc21abffa1785e642d
pendo324 <pendo3247@gmail.com>
2016-04-05 13:12:57 -0400

    Implement nicer editor on search page

--- client/app/controllers/recipe.js
+++ client/app/controllers/recipe.js
@@ -4,6 +4,11 @@ export default Ember.Controller.extend({
   actions: {
     newComment() {
       // do server stuff
+      console.log(this.get("editor").getValue());
+    },
+
+    clearText() {
+      this.get("editor").setValue("");
     }
   }
 });
