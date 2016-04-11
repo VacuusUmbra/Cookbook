648cb514f44b3c1c48e05b17a798419378709c65
Jgonz770 <jgonz770@fiu.edu>
2016-04-10 20:47:33 -0400

    Added files via upload
    Updated comments table to not accept duplicate submissions
    Modified Flag table
    Added Stored Procedures for Delete/Update (Flag updates still needed)

---
+++ db/cookbookdb_flags.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `cookbookdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `cookbookdb`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cookbookdb
+-- ------------------------------------------------------
+-- Server version	5.7.11-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `flags`
+--
+
+DROP TABLE IF EXISTS `flags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `flags` (
+  `fID` int(11) NOT NULL AUTO_INCREMENT,
+  `userFlagged` varchar(45) NOT NULL,
+  `FlaggedBy` varchar(45) NOT NULL,
+  `commentORrecipe` int(1) DEFAULT NULL,
+  PRIMARY KEY (`fID`),
+  UNIQUE KEY `fID_UNIQUE` (`fID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='a list of all registered flags';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `flags`
+--
+
+LOCK TABLES `flags` WRITE;
+/*!40000 ALTER TABLE `flags` DISABLE KEYS */;
+INSERT INTO `flags` VALUES (1,'GollyGo','Jalvo',1),(2,'GollyGo','Jalvo',0),(3,'Javlo','LasterNamerson',1),(4,'GollyGo','LasterNamerson',1),(5,'GollyGo','Jalvo',1),(6,'GollyGo','Ace',0),(7,'GollyGo','Jalvo',0),(8,'GollyGo','Ace',1),(9,'GollyGo','LasterNamerson',1),(10,'GollyGo','LasterNamerson',0),(11,'GollyGo','Jalvo',1),(12,'GollyGo','Jalvo',1);
+/*!40000 ALTER TABLE `flags` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-10 20:04:33
